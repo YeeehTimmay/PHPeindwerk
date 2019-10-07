b988e785db128e148aa7ba8bd10ee91294f90503
YeeehTimmay <jonathan.dom022001@gmail.com>
2019-10-07 09:47:46 +0200

    Day 5 7/10/2019 changed Inschrijving_Prijzen so you can change individual prices

--- Index.php
+++ Index.php
@@ -1,8 +1,8 @@
 <?php
-echo "<head><title>Voetbal Inschrijvingen 2019-2020</title><h1>Voetbal Inschrijvingen 2019-2020</h1></head><body>";
-echo '<link rel="stylesheet" type="text/css" href="style.css"></head>';
+echo "<head><title>Voetbal Inschrijvingen 2019-2020</title><h1>Voetbal Inschrijvingen 2019-2020</h1>";
+echo '<link rel="stylesheet" type="text/css" href="style.css"></head><body>';
 echo '<form method = "post" action = "login.php">';
-echo '<button type= "submit">LOG IN</button>';
+echo '<button type= "submit" class = "right">LOG IN</button>';
 echo '</form> ';
 echo "<a href=/PHPeindwerk/InschrijfFormulier>INSCHRIJVEN</a><br><br>";
 echo "<a href=/PHPeindwerk/Overzicht_Inschrijvingen>OVERZICHT</a><br><br>";
--- Inschrijving_Prijzen.php
+++ Inschrijving_Prijzen.php
@@ -9,35 +9,54 @@ else {
   $link = mysqli_connect("localhost","trainer","penalty","voetbal");
   if ($link) {
     echo '<form action="Inschrijving_Prijzen.php" method="post">';
-    echo 'Prijs inschrijving: <input type = "text" size="30" name="inschrijving"required = "required"><br>';
-    echo 'Prijs trainingspak: <input type = "text" size = "30" name="trainingspak" required ="required"><br>';
-    echo 'Prijs Wedstrijdkledij: <input type = "text" size="30" name="wedstrijdkledij"required = "required"><br>';
-    echo 'Prijs bal: <input type = "text" size = "30" name="balprijs" required ="required"><br></form>';
-    echo '<input type="submit" value="Aanpassen"> ';
+    echo 'Prijs inschrijving: <input type = "text" size="30" name="inschrijving"><br>';
+    echo 'Prijs trainingspak: <input type = "text" size = "30" name="trainingspak" ><br>';
+    echo 'Prijs Wedstrijdkledij: <input type = "text" size="30" name="wedstrijdkledij"><br>';
+    echo 'Prijs bal: <input type = "text" size = "30" name="balprijs" ><br>';
+    echo '<input type="submit" value="Aanpassen"></form> ';
   }
   if (isset($_POST['inschrijving']) and (isset($_POST['trainingspak'])) and (isset($_POST['wedstrijdkledij'])) and (isset($_POST['balprijs']))){
     $PrijsInschrijving = $_POST['inschrijving'];
     $PrijsTraining = $_POST['trainingspak'];
     $PrijsWedstrijd = $_POST['wedstrijdkledij'];
     $PrijsBal = $_POST['balprijs'];
-    if ((empty($PrijsInschrijving)) or (empty($PrijsTraining)) or (empty($PrijsWedstrijd)) or (empty($PrijsBal))) {
+    if ((empty($PrijsBal))) {
     }
-    $query = "update `prijzen` set `item`='balprijs',`prijs`= $PrijsBal where `item`= 'balprijs'";
-    if (mysqli_query($link,$query)) {
-      echo "Bal prijs veranderd naar $PrijsBal<br>";
-  }
-  $query = "update `prijzen` set `item`='inschrijvingsgeld',`prijs`= $PrijsInschrijving where `item`= 'inschrijvingsgeld' ";
+    else {
+      $query = "update `prijzen` set `item`='balprijs',`prijs`= $PrijsBal where `item`= 'balprijs'";
+      if (mysqli_query($link,$query)) {
+        echo "Bal prijs veranderd naar $PrijsBal<br>";
+      }
+    }
+      if (empty($PrijsInschrijving)){
+    }
+    else {
+      $query = "update `prijzen` set `item`='inschrijvingsgeld',`prijs`= $PrijsInschrijving where `item`= 'inschrijvingsgeld' ";
+      if (mysqli_query($link,$query)) {
+        echo "Inschrijvingsprijs veranderd naar $PrijsInschrijving<br>";
+    }
+
+    }
+    if (empty($PrijsTraining)) {
+    }
+    else {
+      $query = "update `prijzen` set `item`='trainingspak',`prijs`= $PrijsTraining where `item`= 'trainingspak' ";
+      if (mysqli_query($link,$query)) {
+        echo "Trainingspak prijs veranderd naar $PrijsTraining<br>";
+      }
+
+    }
+if (empty($PrijsWedstrijd)){
+}
+else {
+  $query = "update `prijzen` set `item`='wedstrijdkledij',`prijs`= $PrijsWedstrijd where `item`= 'wedstrijdkledij' ";
   if (mysqli_query($link,$query)) {
-    echo "Inschrijvingsprijs veranderd naar $PrijsInschrijving<br>";
+    echo "Wedstrijdkledij prijs veranderd naar $PrijsWedstrijd<br>";
+  }
+
 }
-$query = "update `prijzen` set `item`='trainingspak',`prijs`= $PrijsTraining where `item`= 'trainingspak' ";
-if (mysqli_query($link,$query)) {
-  echo "Trainingspak prijs veranderd naar $PrijsTraining<br>";
-}
-$query = "update `prijzen` set `item`='wedstrijdkledij',`prijs`= $PrijsWedstrijd where `item`= 'wedstrijdkledij' ";
-if (mysqli_query($link,$query)) {
-  echo "Wedstrijdkledij prijs veranderd naar$PrijsWedstrijd<br>";
-}
+
+
 }
 }
 echo "<br><a href= Index.php>Terug naar eerste pagina</a>";
